1031114d3aa240c77add4342e769a50a810bc4e6
MrDarkV27 <133194809+MrDarkV27@users.noreply.github.com>
2023-05-16 17:44:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+import time
+
+def make_request():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requestt():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requesttt():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requestttt():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requesttttt():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requesttttttt():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requestx():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+def make_requestp():
+    url = 'https://amodz.online'  
+    response = requests.get(url)
+   
+
+while True:
+    make_request()
+    make_requestt()
+    make_requesttt()
+    make_requestttt()
+    make_requesttttt()
+    make_requestttttt()
+    make_requesttttttt()
+    make_requestx()
+    make_requestp()
+   
